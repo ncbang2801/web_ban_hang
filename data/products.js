160427d8ca12e180e4a9d63833f1b2c388c3158d
ncbang2801 <bangcongbang123@gmail.com>
2024-04-16 19:55:23 +0700

    init project

---
+++ data/products.js
@@ -0,0 +1,410 @@
+var list_products = [{
+    "name": "Banh ngot vi Socola (BanhNgot)",
+    "phanloai": "BanhNgot",
+    "img": "img/products/banhsocola.jpg",
+    "price": "19.000",
+    "star": 4,
+    "rateCount": 36,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Bánh socola ăn vào tha hồ la",
+    },
+    "masp": "Xia0"
+}, {
+    "name": "Banh ngot vi Matcha (BanhNgot)",
+    "phanloai": "Banhngot",
+    "img": "img/products/banhmatcha.jpg",
+    "price": "200.000",
+    "star": 5,
+    "rateCount": 588,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Vị của bánh matcha ăn nhớ cả ba đời ",
+    },
+    "masp": "Opp0"
+},{
+    "name": "Banh ngot vi Dao (BanhNgot)",
+    "phanloai": "Banhngot",
+    "img": "img/products/banhdao.jpg",
+    "price": "200.000",
+    "star": 5,
+    "rateCount": 588,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Vị của bánh matcha ăn nhớ cả ba đời ",
+    },
+    "masp": "Opp0"
+},
+ {
+    "name": "Banh ngot vi dau tay (Banhngot)",
+    "phanloai": "Banhngot",
+    "img": "img/products/banhdau.jpg",
+    "price": "22.000",
+    "star": 5,
+    "rateCount": 68,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Bánh dâu tây ăn sang Tây"
+    },
+    "masp": "Nok0"
+}, {
+    "name": "Banh ngot vi viet quat(Banhngot)",
+    "phanloai": "Banhngot",
+    "img": "img/products/banhvietquat.jpg",
+    "price": "16.000",
+    "star": 4,
+    "rateCount": 21,
+    "promo": {
+        "name": "giamgia",
+        "value": "3.000"
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Bánh việt quất ăn xong là quất"
+    },
+    "masp": "Sam1"
+}, {
+    "name": "Banh ngot vi xoai(Banhngot)",
+    "phanloai": "Banhngot",
+    "img": "img/products/banhxoai.jpg",
+    "price": "12.000",
+    "star": 4,
+    "rateCount": 53,
+    "promo": {
+        "name": "Mới ra mắt",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "Bánh ngọt",
+        "describe": "Bánh xoài ăn nhớ hoài"
+    },
+    "masp": "App0"
+}, {
+    "name": "Banh ngot vi kiwi(Banhngot)",
+    "phanloai": "BanhNgot",
+    "img": "img/products/banhkiwi.jpg",
+    "price": "12.000",
+    "star": 5,
+    "rateCount": 254,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhNgot",
+        "describe": "Kiwi ăn là quy i"
+    },
+    "masp": "Opp1"
+}, {
+    "name": "Banh sinh nhat freefire(Banhsinhnhat)",
+    "phanloai": "BanhSinhNhat",
+    "img": "img/products/banhfreefire.jpg",
+    "price": "180.000",
+    "star": 5,
+    "rateCount": 1000,
+    "promo": {
+        "name": "giamgia",
+        "value": "1000"
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhSinhNhat",
+        "describe": "Freefire sống dai làm huyền thoại "
+    },
+    "masp": "Sam2"
+}, {
+    "name": "Banh sinh nhat lien quan(Banhsinhnhat)",
+    "phanloai": "BanhSinhNhat",
+    "img": "img/products/banhlienquan.png",
+    "price": "12.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhSinhNhat",
+        "describe": "Ăn cũng tạm"
+    },
+    "masp": "App1"
+}, {
+    "name": "Banh sinh nhat plant vs zombie(BanhSinhNhat)",
+    "phanloai": "BanhSinhNhat",
+    "img": "img/products/Banhplantvszombie.webp",
+    "price": "13.000",
+    "star": 3,
+    "rateCount": 7,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhSinhNhat",
+        "describe": "Chưa ăn nhưng có vẻ ngon"
+    },
+    "masp": "App2"
+}, {
+    "name": "Banh lien minh huyen thoai(BanhSinhNhat)",
+    "phanloai": "BanhSinhNhat",
+    "img": "img/products/banhlienminh.png",
+    "price": "16.000",
+    "star": 4,
+    "rateCount": 12,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhSinhNhat",
+        "describe": "Siêu ngon"
+    },
+    "masp": "Xia1"
+}, {
+    "name": "Banh mi op la(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmiopla.jpg",
+    "price": "12.000",
+    "star": 5,
+    "rateCount": 157,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Quá là ngon"
+    },
+    "masp": "Mob0"
+}, {
+    "name": "Banh mi thit nuong(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmithitnuong.jpg",
+    "price": "16.000",
+    "star": 4,
+    "rateCount": 32,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Quá là ngon"
+    },
+    "masp": "Mob1"
+}, {
+    "name": "Banh mi cha lua(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmichalua.jpg",
+    "price": "23.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Quá là ngon"
+    },
+    "masp": "Mob2"
+}, {
+    "name": "Banh mi bo toi(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmibotoi.jpg",
+    "price": "29.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Ăn tạm"
+    },
+    "masp": "Mob3"
+}, {
+    "name": "Banh duc man(BanhMan)",
+    "phanloai": "BanhMan",
+    "img": "img/products/banhduc.jpg",
+    "price": "29.000",
+    "star": 4,
+    "rateCount": 37,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMan",
+        "describe": "Ăn được chứ không ngon lắm"
+    },
+    "masp": "Xia2"
+}, {
+    "name": "Banh gio(BanhMan)",
+    "phanloai": "BanhMan",
+    "img": "img/products/banhgio.jpg",
+    "price": "27.000",
+    "star": 4,
+    "rateCount": 47,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMan",
+        "describe": "Này ngon"
+    },
+    "masp": "Xia3"
+}, {
+    "name": "Banh cuon(BanhMan)",
+    "phanloai": "BanhMan",
+    "img": "img/products/banhcuon.jpg",
+    "price": "18.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMan",
+        "describe": "Này ngon"
+    },
+    "masp": "Hua0"
+}, {
+    "name": "Banh ran man(BanhMan)",
+    "phanloai": "BanhMan",
+    "img": "img/products/banhran.jpg",
+    "price": "22.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMan",
+        "describe": ""
+    },
+    "masp": "Hua1"
+}, {
+    "name": "Banh bong lan sua chua(BanhNgot)",
+    "phanloai": "BanhNgot",
+    "img": "img/products/banhbonglansuachua.jpg",
+    "price": "11.000",
+    "star": 5,
+    "rateCount": 333,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhNgot",
+        "describe": "Ngon nhất trong các món FF thề"
+    },
+    "masp": "Hua2"
+}, {
+    "name": "Banh bong lan cuon(BanhNgot)",
+    "phanloai": "BanhNgot",
+    "img": "img/products/banhbonglancuon.jpg",
+    "price": "32.000",
+    "star": 3,
+    "rateCount": 23,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhNgot",
+        "describe": "Ngon bổ và rẻ"
+    },
+    "masp": "Nok1"
+}, {
+    "name": "Banh mi hoa cuc(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmihoacuc.jpg",
+    "price": "16.000",
+    "star": 5,
+    "rateCount": 470,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Ngon"
+    },
+    "masp": "Sam3"
+}, {
+    "name": "Banh mi den(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmiden.jpg",
+    "price": "23.000",
+    "star": 5,
+    "rateCount": 320,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Ngon"
+    },
+    "masp": "Sam3"
+},{
+    "name": "Banh mi cha ca(BanhMi)",
+    "phanloai": "BanhMi",
+    "img": "img/products/banhmichaca.jpg",
+    "price": "16.000",
+    "star": 3,
+    "rateCount": 4,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "area": "Tất cả cả các chi nhánh cửa hàng",
+        "tags": "BanhMi",
+        "describe": "Ngon"
+    },
+    "masp": "Sam3"
+}]
